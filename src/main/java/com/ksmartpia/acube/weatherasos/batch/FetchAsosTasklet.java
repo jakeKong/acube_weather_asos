630a07d9664d5c9aa8aacec6ac9d4f920f57bdcb
devsurimlee2 <roserote@naver.com>
2021-01-26 15:47:27 +0900

    커밋

--- src/main/java/com/ksmartpia/acube/weatherasos/batch/FetchAsosTasklet.java
+++ src/main/java/com/ksmartpia/acube/weatherasos/batch/FetchAsosTasklet.java
@@ -69,7 +69,7 @@ public class FetchAsosTasklet {
 	 * 대구권 기상정보를 수집하는 API
 	 * 당일 정보는 개인이 사용불가, 전날 데이터를 1시간 단위로 저장함. (00시~ 23시)
 	 * 
-	 */
+	 */  
 	@Scheduled(cron=" 0 0 10 * * *")
 	public void getAsosInfo() throws Exception {
 		System.out.println("ASOS 종관 기상 정보 시작");
